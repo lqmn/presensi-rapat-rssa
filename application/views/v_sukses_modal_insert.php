fc6e0ded8ebdfaa4ecb8a281402ccc795062e067
lqmn <lqm.ariff@gmail.com>
2017-08-13 23:18:28 +0700

    remove modal header text

--- application/views/v_sukses_modal_insert.php
+++ application/views/v_sukses_modal_insert.php
@@ -1,6 +1,6 @@
 <div class="modal-header">
 	<button type="button" class="close" data-dismiss="modal">&times;</button>
-	<h4 class="modal-title">Sukses</h4>
+	<!-- <h4 class="modal-title">Sukses</h4> -->
 </div>
 
 <div class="modal-body">
