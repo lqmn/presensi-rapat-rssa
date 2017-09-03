20473fd942129e391b62595d17ac69148cf6eafa
lqmn <lqm.ariff@gmail.com>
2017-09-03 19:30:22 +0700

    Revert "Merge branch 'master' into nyoba-rekap"
    This reverts commit 9b020a9b635f230a362775b525fbbc852161fb4a, reversing
    changes made to c32d16d4e70de4240d607d7f1e4b82c5aadf3bdb.

--- application/views/v_header_rekap_lembur.php
+++ application/views/v_header_rekap_lembur.php
@@ -1,79 +1,16 @@
-<!DOCTYPE html>
-<head>
-	<title>Presensi</title>
-	<meta name="viewport" content="width=device-width, initial-scale=1.0" />
-
-	<link rel="stylesheet" type="text/css" href="<?php echo base_url("assets/css/bootstrap.css"); ?>">
-	<link rel="stylesheet" type="text/css" href="<?php echo base_url("assets/DataTables/datatables.min.css"); ?>">
-	<link rel="stylesheet" type="text/css" href="<?php echo base_url("assets/css/bootstrap-select.min.css"); ?>">
-	<link rel="stylesheet" type="text/css" href="<?php echo base_url("assets/css/style.css"); ?>">
-	<script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js"></script>
-
-</head>
-<body>
-
-	<div id="bigModal" class="modal fade" role="dialog">
-		<div class="modal-dialog modal-lg">
-			<div id="bigModalContent" class="modal-content">
-				//content from ajax loaded here
-
-			</div>
-		</div>
-	</div>
-
-	<?php $this->load->view('navbar_admin'); ?><br>
-
-	<div class="container text-center">
-		<div class="row content" id="mainContent">
-			<div class="col-sm-12 text-left" id="mainDiv">
-				<h4>Rekap Absen</h4>
-				<form class="form=horizontal" action="<?php echo base_url();?>c_presensi/rekap_lembur/" method="post">
-	<div class="form-group">
-			<label class="control-label col-sm-1" for="pwd">Bulan:</label>
-			<div class="col-sm-11">
-
-				<select class="selectpicker"  name="id_bulan2">
-					
-						<option value="1" >Januari</option>
-						<option value="2" >Februari</option>
-						<option value="3" >Maret</option>
-						<option value="4" >April</option>
-						<option value="5" >Mei</option>
-						<option value="6" >Juni</option>
-						<option value="7" >Juli</option>
-						<option value="8" >Agustus</option>
-						<option value="9" >September</option>
-						<option value="10" >Oktober</option>
-						<option value="11" >November</option>
-						<option value="12" >Desember</option>
-
-				        
-				</select>
-			</div>
-		</div>
-		<br>
-		<br>
-		<br>
-
-		
-
-				<br>
-<input type="submit" id="rekapAbsen" class="btn btn-primary"value="Lihat Rekap Lembur" />
-<br>
-<hr>
-</form>
-				
-				
-				
 
 
-				<br>
-				<table id="tabel" class="table table-striped" cellspacing="0" width="100%">
-					<thead>
-						<tr>
-							<th>NIP</th>
-							<th>NAMA</th>
-							<th>Total Lembur</th>
+			<div class="col-sm-9" style="margin-left:25%">
+				<div class="well">
+					<div width=100% style="float:right;">
 						
-						</tr>
-					</thead>
+					</div>
+					<table class="table table-striped table-bordered" cellspacing="0" width="100%">
+
+							<thead>
+					<tr>
+						<th>NAMA</th>
+						<th>NIP</th>
+						<th>TOTAL JAM LEMBUR</th>
+					</tr>
+				</thead>
